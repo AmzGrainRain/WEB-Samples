db61288b556958fbf257aac6d2b60485c0a65ef4
GrainRain <s3238382606@163.com>
2022-05-10 11:03:29 +0800

    update

--- 浏览器/高仿淘宝首页/js/index.js
+++ 浏览器/高仿淘宝首页/js/index.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * 滚动到一定高度时，显示第二个导航栏
+ * 
+ */
 window.onscroll = () => {
   const top = document.querySelector('body').getBoundingClientRect().top
   if (top < -154) {
@@ -14,62 +19,85 @@ window.onscroll = () => {
  *  淘宝轮播点击事件处理
  * 
  */
+// 监听点击事件
 document.querySelector('#content .box .carousel .tb .indicator-l').addEventListener('click', (e) => carouselLg.scroll(0))
 document.querySelector('#content .box .carousel .tb .indicator-r').addEventListener('click', (e) => carouselLg.scroll(1))
+// 轮播图逻辑
 const carouselLg = {
-  parentEl: document.querySelector('#content .box .carousel .tb .mod'),
+  allowScroll: true,
+  parentEl: document.querySelector('#content .box .carousel .tb .mod'), // 父节点
   firstEl: document.querySelector('#content .box .carousel .tb .mod').firstElementChild.cloneNode(true),
   tabs: document.querySelectorAll('#content .box .carousel .tb .tab a'),
   timer: null,
   min: 0,
   max: 4,
   index: 0,
+  /**
+   * 自动轮播
+   * @param {number} ys 时间（ms）
+   * @param {number} fx 滚动方向 [true 右 | false 左]
+   */
   addTimer: function (ys, fx) {
     this.timer = setInterval(() => {
       this.scroll(fx)
     }, ys)
   },
+  /**
+   * 轮播
+   * @param { boolean } fx 滚动方向
+   */
   scroll: function (fx) {
-    // 判断方向
-    if (fx) {
-      // 索引越界时仍然正常轮播
-      this.index++
-      this.parentEl.style.left = `${this.index * -100}%`
-      this.parentEl.style.transition = '.2s ease'
-      if (this.index > this.max) {
-        // 正常轮播过渡动画结束之后
-        setTimeout(() => {
-          // 切换到第一张
-          this.index = this.min
-          this.parentEl.style.left = 0
+    // 判断是否允许轮播
+    if (this.allowScroll) {
+      // 禁止再次轮播
+      this.allowScroll = false
+      // 判断方向
+      if (fx) {
+        // 索引越界时仍然正常轮播
+        this.index++
+        this.parentEl.style.left = `${this.index * -100}%`
+        this.parentEl.style.transition = '.2s ease'
+        if (this.index > this.max) {
+          // 正常轮播过渡动画结束之后
+          setTimeout(() => {
+            // 切换到第一张
+            this.index = this.min
+            this.parentEl.style.left = 0
+            // 关闭过渡效果
+            this.parentEl.style.transition = 'none'
+          }, 200)
+        }
+      } else {
+        this.index--
+        // 索引越界时
+        if (this.index < this.min) {
+          // 切换到第六张
+          this.index = this.max + 1
+          this.parentEl.style.left = `${this.index * -100}%`
           // 关闭过渡效果
           this.parentEl.style.transition = 'none'
-        }, 200)
-      }
-      // 更新小圆点状态
-      this.tabsUpdate(fx)
-    } else {
-      this.index--
-      // 索引越界时
-      if (this.index < this.min) {
-        // 切换到第六张
-        this.index = this.max + 1
-        this.parentEl.style.left = `${this.index * -100}%`
-        // 关闭过渡效果
-        this.parentEl.style.transition = 'none'
-        setTimeout(() => {
-          this.index = this.max
+          setTimeout(() => {
+            this.index = this.max
+            this.parentEl.style.left = `${this.index * -100}%`
+            this.parentEl.style.transition = '.2s ease'
+          }, 200)
+        } else {
+          // 正常轮播
           this.parentEl.style.left = `${this.index * -100}%`
-          this.parentEl.style.transition = '.2s ease'
-        }, 0)
-      } else {
-        // 正常轮播
-        this.parentEl.style.left = `${this.index * -100}%`
+        }
       }
       // 更新小圆点状态
       this.tabsUpdate(fx)
     }
+    // 1秒后允许再次轮播
+    setTimeout(() => {
+      this.allowScroll = true
+    }, 1000)
   },
+  /**
+   * 跳转到指定轮播图
+   * @param { number } num 指定轮播图索引
+   */
   jumpTo: function (num) {
     // 跳转到指定轮播图
     this.index = num
@@ -78,11 +106,18 @@ const carouselLg = {
     // 更新小圆点状态
     this.tabsUpdate()
   },
+  /**
+   * 清除 tab 的背景颜色
+   */
   tabsClear: function () {
     for (let i = 0, len = this.tabs.length; i < len; i++) {
       this.tabs[i].classList.remove('active')
     }
   },
+  /**
+   * 轮播 tab 
+   * @param { boolean } fx 滚动方向
+   */
   tabsUpdate: function (fx) {
     // 如果是第六张轮播
     if (this.index === this.max + 1) {
@@ -101,6 +136,9 @@ const carouselLg = {
       }
     }
   },
+  /**
+   * 初始化
+   */
   init: function () {
     // 克隆首个轮播图并将其追加到最后
     this.parentEl.appendChild(this.firstEl)
@@ -112,6 +150,7 @@ const carouselLg = {
     this.addTimer(5000, 1)
   },
 }
+
 /**
  * 
  *  天猫轮播点击事件处理
@@ -120,6 +159,7 @@ const carouselLg = {
 document.querySelector('#content .box .carousel .tm .inner-carousel .indicator-l').addEventListener('click', (e) => carouselSm.scroll(0))
 document.querySelector('#content .box .carousel .tm .inner-carousel .indicator-r').addEventListener('click', (e) => carouselSm.scroll(1))
 const carouselSm = {
+  allowScroll: true,
   parentEl: document.querySelector('#content .box .carousel .tm .inner-carousel .mod'),
   firstEl: document.querySelector('#content .box .carousel .tm .inner-carousel .mod').firstElementChild.cloneNode(true),
   label: document.querySelector('#content .box .carousel .tm .title span i'),
@@ -127,52 +167,71 @@ const carouselSm = {
   min: 0,
   max: 4,
   index: 0,
+  /**
+   * 自动轮播
+   * @param {number} ys 时间（ms）
+   * @param {number} fx 滚动方向 [true 右 | false 左]
+   */
   addTimer: function (ys, fx) {
     this.timer = setInterval(() => {
       this.scroll(fx)
     }, ys)
   },
+  /**
+   * 轮播
+   * @param { boolean } fx 滚动方向
+   */
   scroll: function (fx) {
-    // 判断方向
-    if (fx) {
-      // 索引越界时仍然正常轮播
-      this.index++
-      this.parentEl.style.left = `${this.index * -100}%`
-      this.parentEl.style.transition = '.2s ease'
-      if (this.index > this.max) {
-        // 正常轮播过渡动画结束之后
-        setTimeout(() => {
-          // 切换到第一张
-          this.index = this.min
-          this.parentEl.style.left = 0
+    if (this.allowScroll) {
+      // 禁止再次轮播
+      this.allowScroll = false
+      // 判断方向
+      if (fx) {
+        // 索引越界时仍然正常轮播
+        this.index++
+        this.parentEl.style.left = `${this.index * -100}%`
+        this.parentEl.style.transition = '.2s ease'
+        if (this.index > this.max) {
+          // 正常轮播过渡动画结束之后
+          setTimeout(() => {
+            // 切换到第一张
+            this.index = this.min
+            this.parentEl.style.left = 0
+            // 关闭过渡效果
+            this.parentEl.style.transition = 'none'
+          }, 200)
+        }
+      } else {
+        this.index--
+        // 索引越界时
+        if (this.index < this.min) {
+          // 切换到第六张
+          this.index = this.max + 1
+          this.parentEl.style.left = `${this.index * -100}%`
           // 关闭过渡效果
           this.parentEl.style.transition = 'none'
-        }, 200)
-      }
-      // 更新文字显示
-      this.updateLabel(fx)
-    } else {
-      this.index--
-      // 索引越界时
-      if (this.index < this.min) {
-        // 切换到第六张
-        this.index = this.max + 1
-        this.parentEl.style.left = `${this.index * -100}%`
-        // 关闭过渡效果
-        this.parentEl.style.transition = 'none'
-        setTimeout(() => {
-          this.index = this.max
+          setTimeout(() => {
+            this.index = this.max
+            this.parentEl.style.left = `${this.index * -100}%`
+            this.parentEl.style.transition = '.2s ease'
+          }, 0)
+        } else {
+          // 正常轮播
           this.parentEl.style.left = `${this.index * -100}%`
-          this.parentEl.style.transition = '.2s ease'
-        }, 0)
-      } else {
-        // 正常轮播
-        this.parentEl.style.left = `${this.index * -100}%`
+        }
       }
       // 更新文字显示
       this.updateLabel(fx)
     }
+    // 1秒后允许再次轮播
+    setTimeout(() => {
+      this.allowScroll = true
+    }, 1000)
   },
+  /**
+   * 轮播 label 
+   * @param { boolean } fx 滚动方向
+   */
   updateLabel: function (fx) {
     // 如果是第六张轮播
     if (this.index === this.max + 1) {
@@ -183,6 +242,9 @@ const carouselSm = {
       this.label.innerHTML = this.index + 1
     }
   },
+  /**
+   * 初始化
+   */
   init: function () {
     // 克隆首个轮播图并将其追加到最后
     this.parentEl.appendChild(this.firstEl)
@@ -204,7 +266,7 @@ carouselSm.init()
  *  鼠标浮动到轮播时清除自动切换定时器
  * 
  */
-document.querySelector('#content .box .carousel .tb .mod').addEventListener('mouseover', () => {
+document.querySelector('#content .box .carousel .tb').addEventListener('mouseover', () => {
   clearInterval(carouselLg.timer)
 })
 document.querySelector('#content .box .carousel .tm').addEventListener('mouseover', () => {
